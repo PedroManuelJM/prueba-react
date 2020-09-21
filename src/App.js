681268dd9f914abddc4b95811cb30c350e344a52
PedroManuelJM <mpedrojurado@gmail.com>
2020-09-20 23:34:43 -0500

    poniendo mensaje

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Mama y koki perdoname  ....... 
         </p>
         <a
           className="App-link"
