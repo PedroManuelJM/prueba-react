f1c8596f5f912b5cd4b37171a88ef0e527ffaa91
PedroManuelJM <mpedrojurado@gmail.com>
2020-09-20 23:31:19 -0500

    Updates

---
+++ precache-manifest.42eabcec31a6f613c3d57d111efc43a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50fca6e14a554b8a8e7b4e348f662d4",
+    "url": "/prueba-react/index.html"
+  },
+  {
+    "revision": "20d9ce0da5493740e562",
+    "url": "/prueba-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aaadf90d963976121c27",
+    "url": "/prueba-react/static/js/2.ceb98ce5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prueba-react/static/js/2.ceb98ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20d9ce0da5493740e562",
+    "url": "/prueba-react/static/js/main.c638931a.chunk.js"
+  },
+  {
+    "revision": "eeabad2ed39d9dd10d70",
+    "url": "/prueba-react/static/js/runtime-main.ddab31c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prueba-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
